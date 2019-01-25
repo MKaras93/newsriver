c152fbeb6b54d60f7ab569421bc63fd9a01d43c0
Miczi <mjkaras93@gmail.com>
2019-01-25 18:28:23 +0100

    first draft with news api and postgresql

--- supply.py
+++ supply.py
@@ -1,6 +1,12 @@
 from newsapi import NewsApiClient
+import psycopg2
 import json
 # Init
+
+#1. Collect data from query -> returns json with the data.
+#2. Insert data into database/csv, recognising the same news. Same news are discarded.
+#3. Get a collection of undisplayed news from db
+
 
 def load_news():
     newsapi = NewsApiClient(api_key='c644d0c6cff44d31a5180b9cc91c35bc')
@@ -21,7 +27,14 @@ def load_news():
 
     return all_articles
 
+def was_news_loaded(news):
+    """returns true if news is already in the database"""
+    # can be done in database or before loading. If before loading, I have to extract similiar data
+    # news was loaded if url or (headline and source and publication date) are same in old and new.
+    # should be done on SQL side, insert into with conditions
+    # let's setup a postgres db.
 
-def print_articles(news):
-    for article in news['articles']:
-        print(article['publishedAt'] + ' - ' + article['title'] + '\n')
+
+def db_connect():
+    conn = psycopg2.connect(dbname='news', user='postgres', password='postgres')
+    return conn
