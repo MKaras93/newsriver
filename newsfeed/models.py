25646574b4784058b86f957c6ce9c044004e4a2d
miczi <miczipl@gmail.com>
2019-01-28 20:29:44 +0100

    add Tag model

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -3,6 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+def unnull(val):
+    return val if isinstance(val, str) else ''
+
+
+class Tag(models.Model):
+    tag_text = models.CharField(max_length=30, primary_key=True)
+    refresh_freq = models.PositiveIntegerField(default=1)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.tag_text
+
+
 class Article(models.Model):
     author = models.CharField(max_length=100, default='', blank=True, null=False)
     description = models.CharField(max_length=500, default='')
@@ -11,6 +24,7 @@ class Article(models.Model):
     urlToImage = models.URLField(default='')
     publishedAt = models.DateTimeField()
     displayed = models.BooleanField(default=False)
+    tag = models.ManyToManyField(Tag)
 
     def create_from_dict(self, dict_):
         self.author = unnull(dict_['author'])
@@ -24,6 +38,9 @@ class Article(models.Model):
     def __str__(self):
         return self.title[:100]
 
+    def add_tag(self, tag_text):
+        tag = Tag.objects.get(tag_text)
+        self.tag = tag
 
-def unnull(val):
-    return val if isinstance(val, str) else ''
+
+
