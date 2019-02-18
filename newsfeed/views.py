cb2f535975a65597454c68022be69ce841ff8c26
Miczi <mjkaras93@gmail.com>
2019-02-18 21:09:35 +0100

    add queried tags to tag list if they don't exist change priority of queried tags to True

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Tag
 from django.http import HttpResponse
 from django.template import loader
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
 def display_articles_set(request, query=''):
-    query=query.lower()
+    query = query.lower()
+
     if query != '':
         art_set = Article.objects.filter(tag=query, displayed=False).order_by('-publishedAt')[:20]
+        try:
+            tag = Tag.objects.get(pk=query)
+        except (ObjectDoesNotExist):
+            tag = Tag(tag_text=query)
+        finally:
+            tag.priority = True
+            tag.save()
     else:
         art_set = Article.objects.filter(displayed=False).order_by('-publishedAt')[:20]
 
