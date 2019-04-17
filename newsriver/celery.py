1a7e743caef019253f16c27beb7e6e01bb06d14b
miczi <miczipl@gmail.com>
2019-04-17 23:37:20 +0200

    include seperated settings files in celery.py

--- newsriver/celery.py
+++ newsriver/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 #Set default Django settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsriver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsriver.settings.development')
 app = Celery('newsriver')
 # Celery will apply all configuration keys with defined namespace  app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load tasks from all registered apps
